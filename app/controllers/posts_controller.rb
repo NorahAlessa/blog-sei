01854939f72f59f5842dd42e97f862d5cb7db7e8
Norah ALessa <inourahalessa@gmail.com>
2019-07-22 09:35:20 +0300

    comment model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
         @post=Post.new
      end 
 
+
      def create 
         @post=Post.new(post_params)
 
@@ -21,6 +22,26 @@ class PostsController < ApplicationController
         @post =Post.find(params[:id])
      end
    
+     def update
+@post =Post.find(params[:id])
+if @post.update(post_params)
+    redirect_to @post
+else 
+    render 'edit'
+end
+     end
+
+     def edit 
+@post =Post.find(params[:id])
+     end
+
+     def destroy
+@post =Post.find(params[:id])
+@post.destroy
+
+redirect_to posts_path
+     end
+
      private 
      def post_params 
         params.require(:post).permit(:title, :content)
